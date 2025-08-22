69663db1d0b07e6cf4090dda42086236be909eec
Your Name <segodimo@gmail.com>
2025-08-21 21:00:47 -0300

    ajuste eixos para l e r automatico

--- esp/config.py
+++ esp/config.py
@@ -3,6 +3,36 @@ import ubinascii
 
 
 # ============================================================
+# PINAGEM (ESQUERDA E DIREITA)
+# ============================================================
+PINOS_R = 13,12,14,27,4,33
+INDEX_MAP_R = 0,1,2,3,4,5
+PINOS_VIB_R = 26
+
+PINOS_L = 12,13,14,27,4,33
+INDEX_MAP_L = 0,1,2,4,3,5
+PINOS_VIB_L = 26
+
+# ============================================================
+# IDENTIFICAÇÃO DO CHIP / DEFINIÇÃO DO LADO
+# ============================================================
+chip_id = ubinascii.hexlify(machine.unique_id()).decode()
+print("Chip ID:", chip_id)  # Exemplo: '240ac4083456'
+
+# IDs conhecidos dos dois lados
+alesp = '083af27f9c38'
+aresp = '78e36d170944'
+
+# Define se este chip é o lado L (0) ou R (1)
+THIS_IS = 0 if chip_id == alesp else 1
+print("THIS_IS:", THIS_IS)
+
+# INDEX MAP final (depende do lado detectado)
+INDEX_MAP_POTS = list(INDEX_MAP_L if THIS_IS == 0 else INDEX_MAP_R)
+
+
+
+# ============================================================
 # CONFIGURAÇÕES DE TOUCH
 # ============================================================
 CALIB_FILE = "calib.json"
@@ -19,11 +49,14 @@ DEBOUNCE_COUNT  = 2 # Leituras consecutivas para confirmar toque
 # CONFIGURAÇÕES DO GIROSCÓPIO
 # ============================================================
 SAMPLES = 5       # Amostras para suavisar a curva do giroscópio
-LIMGYRO       = 14000   # 8000 (sensível) | 20000 (menos sensível)
-THRES_PERCENT  = 0.1     # 0.05 (5%) | 0.2 (20%)
-GY1, GY2       = 1, 0    # Ordem dos eixos: X depois Y
-INVERT_X       = True    # Inverter sentido do eixo X
-INVERT_Y       = False    # Inverter sentido do eixo Y
+LIMGYRO = 14000   # 8000 (sensível) | 20000 (menos sensível)
+THRES_PERCENT = 0.1     # 0.05 (5%) | 0.2 (20%)
+GY1, GY2 = 1, 0    # Ordem dos eixos: X depois Y
+
+if THIS_IS == 0:
+    INVERT_X, INVERT_Y = False, True  # T,M Inverter sentido do eixo
+else:
+    INVERT_X, INVERT_Y = True, True   # T,M Inverter sentido do eixo
 
 # ============================================================
 # CONTROLE DE PASSOS / RESET
@@ -48,34 +81,6 @@ VIBRAR_LONGO = 250      # 200 para step == 0
 VIBRAR_ALERTA = 300     # 300 para step == 1
 
 
-# ============================================================
-# PINAGEM (ESQUERDA E DIREITA)
-# ============================================================
-PINOS_R = 13,12,14,27,4,33
-INDEX_MAP_R = 0,1,2,3,4,5
-PINOS_VIB_R = 26
-
-PINOS_L = 12,13,14,27,4,33
-INDEX_MAP_L = 0,1,2,4,3,5
-PINOS_VIB_L = 26
-
-
-# ============================================================
-# IDENTIFICAÇÃO DO CHIP / DEFINIÇÃO DO LADO
-# ============================================================
-chip_id = ubinascii.hexlify(machine.unique_id()).decode()
-print("Chip ID:", chip_id)  # Exemplo: '240ac4083456'
-
-# IDs conhecidos dos dois lados
-alesp = '083af27f9c38'
-aresp = '78e36d170944'
-
-# Define se este chip é o lado L (0) ou R (1)
-THIS_IS = 0 if chip_id == alesp else 1
-print("THIS_IS:", THIS_IS)
-
-# INDEX MAP final (depende do lado detectado)
-INDEX_MAP_POTS = list(INDEX_MAP_L if THIS_IS == 0 else INDEX_MAP_R)
 
 
 # ============================================================
